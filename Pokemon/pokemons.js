f7160cbaf00e5d0b68eb313ab80dddbda33516f1
Racconcity <55491998+Racconcity@users.noreply.github.com>
2019-12-03 12:42:51 +0100

    Update pokemons.js

--- Pokemon/pokemons.js
+++ Pokemon/pokemons.js
@@ -24,8 +24,18 @@ module.exports.run = async(client, message, args) => {
         "Tangela.png","Kangaskhan.png","Horsea.png","Seadra.png","Goldeen.png","Seaking.png","Staryu.png","Starmie.png","Mr Mime.png","Scyther.png","Jynx.png",
         "Electabuzz.png","Magmar.png","Pinsir.png","Tauros.png","Magikarp.png","Gyarados.png","Lapras.png","Ditto.png","Eevee.png","Vaporeon.png","Jolteon.png",
         "Flareon.png","Porygon.png","Omanyte.png","Omastar.png","Kabuto.png","Kabutops.png","Aerodactyl.png","Snorlax.png","Articuno.png","Zapdos.png","Moltres.png",
-        "Dratini.png","Dragonair.png","Dragonite.png","Mewtwo.png","Mew.png"
+        "Dratini.png","Dragonair.png","Dragonite.png","Mewtwo.png","Mew.png",
         /*Segunda gen*/
+        "Chikorita.png","Bayleef.png","Meganium.png","Cyndaquil.png","Quilava.png","Typhlosion.png","Totodile.png","Croconaw.png","Feraligatr.png","Sentret.png",
+        "Furret.png","Hoothoot.png","Noctowl.png","Ledyba.png","Ledian.png","Spinarak.png","Ariados.png","Crobat.png","Chinchou.png","Lanturn.png","Pichu.png",
+        "Cleffa.png","Igglybuff.png","Togepi.png","Togetic.png","Natu.png","Xatu.png","Mareep.png","Flaaffy.png","Ampharos.png","Bellossom.png","Marill.png","Azumarill.png",
+        "Sudowoodo.png","Politoed.png","Hoppip.png","Skiploom.png","Jumpluff.png","Aipom.png","Sunkern.png","Sunflora.png","Yanma.png","Wooper.png","Quagsire.png",
+        "Espeon.png","Umbreon.png","Murkrow.png","Slowking.png","Misdreavus.png","Unown.png","Wobbuffet.png","Girafarig.png","Pineco.png","Forretress.png","Dunsparce.png",
+        "Gligar.png","Steelix.png","Snubbull.png","Granbull.png","Qwilfish.png","Scizor.png","Shuckle.png","Heracross.png","Sneasel.png","Teddiursa.png","Ursaring.png",
+        "Slugma.png","Magcargo.png","Swinub.png","Piloswine.png","Corsola.png","Remoraid.png","Octillery.png","Delibird.png","Mantine.png","Skarmory.png","Houndour.png",
+        "Houndoom.png","Kingdra.png","Phanpy.png","Donphan.png","Porygon2.png","Stantler.png","Smeargle.png","Tyrogue.png","Hitmontop.png","Smoochum.png","Elekid.png",
+        "Magby.png","Miltank.png","Blissey.png","Raikou.png","Entei.png","Suicune.png","Larvitar.png","Pupitar.png","Tyranitar.png","Lugia.png","Ho-Oh.png","Celebi.png"
+        /*Tercera gen*/
     ]
     const img = randomimg[Math.floor(Math.random() * randomimg.length - 1 + 1)]
     message.channel.send("El pokémon es: " + img.replace(/\.[^.$]+$/, ''))
